a7b85576fa66909beb935dac26499e915881727f
Nathan Holdom <nathanholdom1@gmail.com>
2020-06-24 22:08:59 -0400

    Doing some testing, now able to access pewdiepie channel.

--- source.py
+++ source.py
@@ -1,5 +1,35 @@
+#!/usr/bin/env
+
+import argparse
+
+from googleapiclient.discovery import build
+
+
+def auth(key):
+    return build('youtube', 'v3', developerKey=key)
+
+
+def getPewdiepieChannel(youtube):
+    request = youtube.channels().list(
+        part='statistics',
+        forUsername='pewdiepie'
+    )
+
+    return request.execute()
+
+
 def main():
-    print("test")
+    parser = argparse.ArgumentParser(description='Start script with api key.')
+    parser.add_argument('key',
+                        help='The api key of the youtube application')
+    args = parser.parse_args()
+
+    youtube = auth(args.key)
+    pewdiepie = getPewdiepieChannel(youtube)
+
+    print(pewdiepie)
+
+
 
 if __name__ == "__main__":
     main()
