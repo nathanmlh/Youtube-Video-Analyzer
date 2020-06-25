2704e6afc90702d4b70e34f733abd23251f24c15
Nathan Holdom <nathanholdom1@gmail.com>
2020-06-24 23:12:15 -0400

    More testing, now will be able to start coding with a purpose.

--- source.py
+++ source.py
@@ -10,9 +10,37 @@ def auth(key):
 
 
 def getPewdiepieChannel(youtube):
+    """
+    This function returns pewdiepie's channel from youtube
+    Takes in a build object that should be youtube
+    """
     request = youtube.channels().list(
-        part='statistics',
+        part='contentDetails',
         forUsername='pewdiepie'
+    )
+
+    return request.execute()
+
+
+def getPlaylist(youtube, id):
+    """
+    Gets playlist from id
+    :param youtube: The youtube instance we are using
+    :param id: The id of the playlist we want to get
+    :return: playlist item
+    """
+    request = youtube.playlistItems().list(
+        part='contentDetails',
+        playlistId=id,
+    )
+
+    return request.execute()
+
+
+def getVideoNames(youtube, list_of_video_names):
+    request = youtube.videos().list(
+        part='statistics',
+        id=list_of_video_names
     )
 
     return request.execute()
@@ -27,8 +55,21 @@ def main():
     youtube = auth(args.key)
     pewdiepie = getPewdiepieChannel(youtube)
 
-    print(pewdiepie)
+    videos_id = pewdiepie['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+    print("The video id for pewdiepie's videos: ", videos_id)
 
+    print()
+    print('Now the videos')
+    playlist = getPlaylist(youtube, videos_id)
+
+    print()
+    for item in playlist['items']:
+        print(item)
+    video_ids = [video['contentDetails']['videoId'] for video in playlist['items']]
+
+    print(video_ids)
+
+    print(getVideoNames(youtube, video_ids))
 
 
 if __name__ == "__main__":
